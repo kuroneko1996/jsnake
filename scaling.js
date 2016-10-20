bbc751e9cdfd89b7d915c050848c29c188c1dd4f
kuroneko1996 <kuronekoruri1996@yandex.ru>
2016-10-21 00:45:32 +0300

    scale buttons up on retina displays

--- scaling.js
+++ scaling.js
@@ -9,5 +9,11 @@
 	if (document.documentElement.clientWidth < 640 && screen_width >= 1080) {
 		var viewport = document.querySelector("meta[name=viewport]");
 		viewport.setAttribute('content', 'width=640, user-scalable=0');
+
+		var fileref = document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", "hdpr.css");
+        document.getElementsByTagName("head")[0].appendChild(fileref);
 	}
 })();
